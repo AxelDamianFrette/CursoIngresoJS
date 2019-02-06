678f665ea9c9d07401e656698516dc97536cc069
alumno <alumno>
2019-02-06 12:20:40 -0300

    termine

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	/* alskdsad */ 
+	var sueldo;
+	var descuento;
+	var sueldoFinal;
+
+	sueldo= importe.value;
+	sueldo=parseInt(sueldo);
+
+	descuento=sueldo *25/100;
+
+	sueldoFinal= sueldo - descuento;
+
+	resultado.value=( sueldoFinal );
+	alert("su importe es: " + sueldoFinal);
 }
+
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -1,9 +1,19 @@
-/*Debemos lograr tomar el importe por ID ,
+ /*Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldoInicial;
+	var aumento;
+	var sueldoFinal;
 
-	
+	sueldoInicial= sueldo.value;
+	sueldoInicial=parseInt(sueldoInicial);
+
+	aumento=sueldoInicial *10/100;
+
+	sueldoFinal= sueldoInicial + aumento;
+
+	resultado.value=( sueldoFinal );
 }
