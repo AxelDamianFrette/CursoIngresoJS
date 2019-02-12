d2487036b4c5ced87e50a801981800ff052fecfe
alumno <alumno>
2019-02-12 12:15:19 -0300

    5 terminada

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -5,10 +5,18 @@ function mostrar()
     edadIngresada= edad.value;
     edadIngresada= parseInt(edadIngresada);
     
-    if(edadIngresada<17 || >13)
+    if(edadIngresada>17 )
     {
     	alert("Usted no es adolecente");
-    }
+    }	
+    	if(edadIngresada<13)
+    	{
+    		alert("Usted no es adolecente");
+    	}
+    	
+
+
+    
         
        
 
