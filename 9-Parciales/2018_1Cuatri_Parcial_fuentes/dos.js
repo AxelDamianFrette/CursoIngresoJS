9dc8694e39df65d74b6feb397a6d770ca6a4b334
alumno <alumno>
2019-02-13 09:29:15 -0300

    numero dos terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,18 @@
 function mostrar()
+/*alert("el precio del producto en efectivo es :$ xxxx, con tarjeta tiene un recargo del 10% que seria $xx de recargo, costandole fianl $xxx") */ 
+
 {
+	var precioProducto;
+	var recargoTarjeta;
+	var precioFinal;
+
+	precioProducto=prompt("ingrese el precio del producto");
+	precioProducto=parseInt(precioProducto);
+
+	recargoTarjeta= precioProducto *10/100;
+
+	precioFinal= precioProducto + recargoTarjeta;
+
+	alert("el precio del producto en efectivo es "+ precioProducto +" con tarjeta tiene un recargo del 10% que seria " + recargoTarjeta + " de recargo, costandole finalmente" + precioFinal);
   
 }
