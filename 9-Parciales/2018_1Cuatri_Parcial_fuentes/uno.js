99959ff953108d0064582efc981c2f489ce678dc
alumno <alumno>
2019-02-13 09:17:15 -0300

    uno

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,21 @@
+/*Ustedes son xxxx y xxxx , sus edades son xx y xx, y la suma de sus edades es xx) */
 
 function mostrar()
 {
+	var persona1;
+	var persona2;
+	var edad1;
+	var edad2;
+	var suma;
 
-}
+     persona1 =prompt("ingrese su nombre aqui");
+     edad1=prompt("ingrese edad de "+ persona1 );
+     edad1=parseInt(edad1);
+
+     persona2=prompt("ingrese su nombre aqui");
+     edad2=prompt("ingrese edad de " + persona2);
+     edad2=parseInt(edad2);
+
+     suma= edad1+edad2;
+
+
