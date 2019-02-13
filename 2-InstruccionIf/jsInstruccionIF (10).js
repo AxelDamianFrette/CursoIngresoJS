86f2700f93056e0fd409d0d3bcae5e30a218cde2
alumno <alumno>
2019-02-13 12:21:11 -0300

    commit numero random 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,30 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom;
+	var mensaje;
+
+	numeroRandom= Math.floor((Math.random() * 10) + 1);
+	//alert(numeroRandom); 
+    console.log(numeroRandom); 
+
+    if(numeroRandom>8)
+    {
+    	mensaje="EXCELENTE";
+    }else{
+     
+        if(numeroRandom>3){
+     	 mensaje="APROBÓ";
+     }
+     if(numeroRandom<4)
+      {
+     	mensaje="Vamos, la proxima se puede";
+      }
+    }
+
+     alert(mensaje);
+     	
+     	
+     
 
 }//FIN DE LA FUNCIÓN
